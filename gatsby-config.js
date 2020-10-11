1a50fb06f10b677a9ac4bb3cc89ab2980987080c
andretwx <andretai.wx@gmail.com>
2020-10-11 17:51:44 +0800

    Modified description metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `HairSalon`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `A sample website for hair salons by Andre Tai.`,
     author: `Andre Tai`,
   },
   plugins: [
